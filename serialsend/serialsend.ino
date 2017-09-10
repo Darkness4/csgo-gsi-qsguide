5df2d67cb621be96d039d48017695a8abdb9c81b
Marc NGUYEN <nguyen_marc@live.fr>
2017-09-10 19:41:52 +0200

    LF and fixes.

--- serialsend/serialsend.ino
+++ serialsend/serialsend.ino
@@ -15,20 +15,20 @@ int adc_key_in  = 0;
 int read_LCD_buttons()
 {
   // For V1.1 us this threshold
-  if (adc_key_in < 50)   return btnRIGHT;  
-  if (adc_key_in < 250)  return btnUP; 
-  if (adc_key_in < 450)  return btnDOWN; 
-  if (adc_key_in < 650)  return btnLEFT; 
-  if (adc_key_in < 850)  return btnSELECT;  
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 250)  return btnUP;
+  if (adc_key_in < 450)  return btnDOWN;
+  if (adc_key_in < 650)  return btnLEFT;
+  if (adc_key_in < 850)  return btnSELECT;
 
   // For V1.0 comment the other threshold and use the one below:
   /*
-    if (adc_key_in < 50)   return btnRIGHT;  
-    if (adc_key_in < 195)  return btnUP; 
-    if (adc_key_in < 380)  return btnDOWN; 
-    if (adc_key_in < 555)  return btnLEFT; 
-    if (adc_key_in < 790)  return btnSELECT;   
-  */  
+    if (adc_key_in < 50)   return btnRIGHT;
+    if (adc_key_in < 195)  return btnUP;
+    if (adc_key_in < 380)  return btnDOWN;
+    if (adc_key_in < 555)  return btnLEFT;
+    if (adc_key_in < 790)  return btnSELECT;
+  */
   return btnNONE;  // when all others fail, return this...
 }
 
